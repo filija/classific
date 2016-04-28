a518871c6570398fd360aed94047c2ae77944858
Filip Denk <filip.denk@gmail.com>
2016-04-28 12:35:32 +0200

    audioread
    zmena stare funkce wavread na novou funkci audioread

--- lib/wav16khz2mfcc.m
+++ lib/wav16khz2mfcc.m
@@ -4,6 +4,6 @@ D=dir([dir_name '/*.wav']);
 file_names = {D.name};
 for ii=1:length(file_names)
   disp(['Processing file: ' dir_name '/' file_names{ii}])
-  s = wavread([dir_name '/' file_names{ii}]);
+  s = audioread([dir_name '/' file_names{ii}]); % kdyby nefungovalo tak wavread
   features{ii} = mfcc(s, 400, 240, 512, 16000, 23, 13);
 end
